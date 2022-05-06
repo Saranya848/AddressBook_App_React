c20d4b1a06414f24b56a9a385b2a638f22de943d
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-05-06 10:01:00 +0530

    Employee Data Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import ContactForm from './components/bookdata/contactform'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!!</h1>
+      <ContactForm/>
     </div>
   );
 }
