512697605819481b57765c35b5a7101322bbd908
SaranyaSiripurapu <Saranyasripurapu@gmail.com>
2022-05-11 16:47:48 +0530

    Editing and deleting data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import AddressBookForm from "./components/book-form/addressbook";
+import Dashboard from "./components/dashboard/dashboard";
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+          <div className="App">
+              <Switch>
+                  <Route exact path="/" component={Dashboard}/>
+                  <Route exact path="/form" component={AddressBookForm}/>
+                  <Route exact path="/form/:id" component={AddressBookForm}/>
+              </Switch>
+          </div>
+      </Router>
   );
 }
 
